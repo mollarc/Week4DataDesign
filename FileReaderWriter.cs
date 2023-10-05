dd4908740b7424576f4c63af1f32b3fb09da7701
mollarc <mollark@live.com>
2023-10-04 20:10:00 -0500

    Add project files.

---
+++ FileReaderWriter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week4DataDesign
+{
+    internal class FileReaderWriter
+    {
+        string directory = Directory.GetCurrentDirectory();
+
+        public FileReaderWriter()
+        {
+
+        }
+        public void ReadAndWriteTextFile()
+        {
+            string[] line;
+            StreamReader reader = new StreamReader(directory + "\\Data\\SamplePipe.txt");
+            FileStream file = File.Create(directory + "\\Data\\SamplePipe_out.txt");
+            StreamWriter writer = new StreamWriter(file);
+            int lineNum = 1;
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine().Split('|');
+                int fieldNum = 0;
+                foreach(string field in line)
+                {
+                    fieldNum++;
+                    writer.Write($"Line#{lineNum} :Field#{fieldNum}={field}");
+                    if (fieldNum == line.Length)
+                    {
+                        fieldNum = 0;
+                        writer.WriteLine();
+                    }
+                    else
+                    {
+                        writer.Write(" ==>");
+                    }
+                }
+                lineNum++;
+            }
+            reader.Close();
+            writer.Close();
+        }
+        public void ReadAndWriteCSVFile()
+        {
+            string[] line;
+            StreamReader reader = new StreamReader(directory + "\\Data\\SampleCSV.csv");
+            FileStream file = File.Create(directory + "\\Data\\SampleCSV_out.txt");
+            StreamWriter writer = new StreamWriter(file);
+            int lineNum = 1;
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine().Split(',');
+                int fieldNum = 0;
+                foreach (string field in line)
+                {
+                    fieldNum++;
+                    writer.Write($"Line#{lineNum} :Field#{fieldNum}={field}");
+                    if (fieldNum == line.Length)
+                    {
+                        fieldNum = 0;
+                        writer.WriteLine();
+                    }
+                    else
+                    {
+                        writer.Write(" ==>");
+                    }
+                }
+                lineNum++;
+            }
+            reader.Close();
+            writer.Close();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Week4DataDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReaderWriter readerWriter = new FileReaderWriter();
+            readerWriter.ReadAndWriteTextFile();
+            readerWriter.ReadAndWriteCSVFile();
+        }
+    }
+}
