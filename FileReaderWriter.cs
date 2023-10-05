022a2c8ba26fb9985fd7f44bbedc79b90c8b7cc6
mollarc <mollark@live.com>
2023-10-04 20:12:12 -0500

    a

--- FileReaderWriter.cs
+++ FileReaderWriter.cs
@@ -15,7 +15,7 @@ namespace Week4DataDesign
         {
 
         }
-        public void ReadAndWriteTextFile()
+        public void ReadAndWriteTextFile()//Reads a text file and outputs a text file
         {
             string[] line;
             StreamReader reader = new StreamReader(directory + "\\Data\\SamplePipe.txt");
@@ -30,7 +30,7 @@ namespace Week4DataDesign
                 {
                     fieldNum++;
                     writer.Write($"Line#{lineNum} :Field#{fieldNum}={field}");
-                    if (fieldNum == line.Length)
+                    if (fieldNum == line.Length)// Resets fieldnum and starts a new line for the next set of information
                     {
                         fieldNum = 0;
                         writer.WriteLine();
@@ -45,7 +45,7 @@ namespace Week4DataDesign
             reader.Close();
             writer.Close();
         }
-        public void ReadAndWriteCSVFile()
+        public void ReadAndWriteCSVFile()// Reads a CSV file and outputs a text file
         {
             string[] line;
             StreamReader reader = new StreamReader(directory + "\\Data\\SampleCSV.csv");
